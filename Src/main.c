9413da49bade37d9970a30c4ca2b850e7730d26b
12766866@qq.com <12766866@qq.com>
2020-05-02 14:31:31 +0800

    使用queue

--- Src/main.c
+++ Src/main.c
@@ -40,12 +40,14 @@
 
 /* Private define ------------------------------------------------------------*/
 /* USER CODE BEGIN PD */
-xTaskHandle TaskAboutPrintf;
+
 /* USER CODE END PD */
 
 /* Private macro -------------------------------------------------------------*/
 /* USER CODE BEGIN PM */
 
+	xQueueHandle xQueue;
+	TaskHandle_t TaskAboutPrintf;
 /* USER CODE END PM */
 
 /* Private variables ---------------------------------------------------------*/
@@ -59,35 +61,85 @@ int fputc(int ch,FILE *f)
 	 return ch;
 }
 
-
 void my_Task_LED3(void * pvParameters){
+	long lValueToSend = (long)pvParameters;
+	portBASE_TYPE xStatus;
 	while(1){
 		HAL_GPIO_TogglePin(GPIOD, LED3_Pin);
-//		vTaskDelay(1000);
-		printf("Hello World 1\n"); 
+		printf("I am alive 100 \r\n"); 
+//		taskYIELD();
+		xStatus = xQueueSendToBack( xQueue, &lValueToSend, 0 );
+		if( xStatus != pdPASS )
+		{
+				/* 发送操作由于队列满而无法完成 – 这必然存在错误，因为本例中的队列不可能满。 */
+				printf( "ERROR   Could not send to the queue.\r\n" );
+		}
+		
+		
+		vTaskDelay(200);
 	}
 }
+
 
 void my_Task_LED4(void * pvParameters){
+	long lValueToSend = (long)pvParameters;
+	portBASE_TYPE xStatus;
 	while(1){
 		HAL_GPIO_TogglePin(GPIOD, LED4_Pin);
-		printf("Hello World 2 \n"); 
-//		vTaskDelay(1200);//
-	}
-}
-
-
-
-void my_Task_Printf(void * pvParameters)
-{
-	while(1){
-		for(unsigned int i = 0;i<=20;i++)
+		printf("I am alive 200 \r\n"); 
+//		taskYIELD();
+		xStatus = xQueueSendToBack( xQueue, &lValueToSend, 0 );
+		if( xStatus != pdPASS )
 		{
-			printf("this is the task 3 printf");
-			vTaskDelay(200);//2
+				/* 发送操作由于队列满而无法完成 – 这必然存在错误，因为本例中的队列不可能满。 */
+				printf( "ERROR   Could not send to the queue.\r\n" );
 		}
+		
+		
+		vTaskDelay(1000);
 	}
 }
+
+
+
+static void vReceiverTask( void *pvParameters )
+{
+/* 声明变量，用于保存从队列中接收到的数据。 */
+long lReceivedValue;
+portBASE_TYPE xStatus;
+const portTickType xTicksToWait = 100 / portTICK_RATE_MS;
+/* 本任务依然处于死循环中。 */
+	for( ;; )
+	{
+		/* 此调用会发现队列一直为空，因为本任务将立即删除刚写入队列的数据单元。 */
+		if( uxQueueMessagesWaiting( xQueue ) != 0 )
+		{
+			printf( "Queue should have been empty!\r\n" );
+		}
+		/* 从队列中接收数据
+		第一个参数是被读取的队列。队列在调度器启动之前就被创建了，所以先于此任务执行。
+		第二个参数是保存接收到的数据的缓冲区地址，本例中即变量lReceivedValue的地址。此变量类型与
+		队列数据单元类型相同，所以有足够的大小来存储接收到的数据。
+		第三个参数是阻塞超时时间 – 当队列空时，任务转入阻塞状态以等待队列数据有效。本例中常量
+		portTICK_RATE_MS用来将100毫秒绝对时间转换为以系统心跳为单位的时间值。
+		*/
+		xStatus = xQueueReceive( xQueue, &lReceivedValue, portMAX_DELAY );
+			if( xStatus == pdPASS )
+			{
+				/* 成功读出数据，打印出来。 */
+				printf( "Received = %ld \r\n " ,lReceivedValue);
+			}
+			else
+			{
+				/* 等待100ms也没有收到任何数据。
+				必然存在错误，因为发送任务在不停地往队列中写入数据 */
+				printf( "ERROR Could not receive from the queue.\r\n" );
+			}
+	}
+}
+
+
+
 /* USER CODE END PV */
 
 /* Private function prototypes -----------------------------------------------*/
@@ -98,7 +150,6 @@ void SystemClock_Config(void);
 
 /* Private user code ---------------------------------------------------------*/
 /* USER CODE BEGIN 0 */
-
 /* USER CODE END 0 */
 
 /**
@@ -132,12 +183,29 @@ int main(void)
   MX_USART3_Init();
   /* USER CODE BEGIN 2 */
 
-		xTaskCreate(my_Task_LED3, (const char *)"LED3", configMINIMAL_STACK_SIZE, NULL, tskIDLE_PRIORITY + 3, NULL);
-		xTaskCreate(my_Task_LED4, (const char *)"LED4", configMINIMAL_STACK_SIZE, NULL, tskIDLE_PRIORITY + 4, NULL);	
-		xTaskCreate(my_Task_Printf, (const char *)"Printf Some Message", configMINIMAL_STACK_SIZE, NULL, tskIDLE_PRIORITY + 2, &TaskAboutPrintf);
+	/* 创建的队列用于保存最多5个值，每个数据单元都有足够的空间来存储一个long型变量 */
+	xQueue = xQueueCreate( 5, sizeof( long ) );
+		if( xQueue != NULL )
+		{
+				/* 创建两个写队列任务实例，任务入口参数用于传递发送到队列的值。所以一个实例不停地往队列发送
+				100，而另一个任务实例不停地往队列发送200。两个任务的优先级都设为1。 */
+				xTaskCreate( my_Task_LED3, "Sender1", 1000, ( void * ) 100, 1, NULL );
+				xTaskCreate( my_Task_LED4, "Sender1", 1000, ( void * ) 200, 1, NULL );
+				/* 创建一个读队列任务实例。其优先级设为2，高于写任务优先级 */
+				xTaskCreate( vReceiverTask, "Receiver", 1000, NULL, 2, NULL );
+				/* 启动调度器，任务开始执行 */
+				vTaskStartScheduler();
+		}
+		else
+		{
+				/* 队列创建失败*/
+				while(1);
+		}
 
-	//	vTaskDelete(TaskAboutPrintf);
+
+		xTaskCreate(my_Task_LED3, (const char *)"LED3", configMINIMAL_STACK_SIZE, NULL, 1, NULL);
 		vTaskStartScheduler();
+		
   /* USER CODE END 2 */
 
   /* Infinite loop */
